fb898876c215e3ff4b406ea02b0add8b912a20de
Yui <dev@sleepyyui.com>
2025-01-02 19:38:26 +0100

    update dashboard

--- dashboard/build/server/nodes/4.js
+++
@@ -1,7 +0,0 @@
-import * as module from '../entries/pages/login.svelte.js';
-
-export { module };
-export const index = 4;
-export const file = '_app/immutable/pages/login.svelte-588eefff.js';
-export const imports = ["_app/immutable/pages/login.svelte-588eefff.js","_app/immutable/chunks/index-dd217174.js","_app/immutable/chunks/Rolling-b09130bd.js"];
-export const stylesheets = ["_app/immutable/assets/login-80337be0.css","_app/immutable/assets/Rolling-2d2c9fb9.css"];
